0de37453ba87170e2443bb91c83f8d372f75f3de
siva <selvamsiva4@gmail.com>
2025-06-25 17:07:58 +0530

    first commit

---
+++ if.js
@@ -0,0 +1,15 @@
+let name = "Sunil";           // String
+let age = 25;                 // Number
+let isStudent = true;         // Boolean
+let address;                  // Undefined
+let score = null;             // Null
+let hobbies = ["music", "sports"]; // Array
+let person = { name: "Sunil", age: 25 }; // Object
+
+console.log(typeof name);      // "string"
+console.log(typeof age);       // "number"
+console.log(typeof isStudent); // "boolean"
+console.log(typeof address);   // "undefined"
+console.log(typeof score);     // "object" (special case for null)
+console.log(typeof hobbies);   // "object"
+console.log(typeof person);    //
---
+++ suniljavascript.js
@@ -0,0 +1,26 @@
+console.log("hi this is hw we print");
+
+let x = 78;
+console.log(x , typeof x);
+console.log("---------------------------------------------------------");
+
+x = "sunil";
+console.log(x , typeof x);
+console.log("---------------------------------------------------------");
+
+x = 3.14;
+console.log(x , typeof x);
+console.log("---------------------------------------------------------");
+
+x = [1,2,3,4,5];
+console.log(x , typeof x);
+console.log("---------------------------------------------------------");
+
+function sayHi(name){
+console.log("hi " + name + " welcome to worley ventures ");
+}
+
+sayHi("sunil");
+sayHi("sachin");
+sayHi("sivaprasad");
+
